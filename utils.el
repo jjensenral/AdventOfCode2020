d329829bf9373afc041f9d1bec055e7d80da635d
Jens Jensen <jens.jensen@stfc.ac.uk>
2020-12-14 13:36:07 +0000

    Need 1 save-current-buffer

--- utils.el
+++ utils.el
@@ -39,41 +39,39 @@ Returns a cons of seconds, and the return value"
 ;;; This should be slightly safer than expecting the substring matches
 ;;; to return nil, as they might legitimately return nil on a failed
 ;;; substring (with \| alternatives).
-
+;;;
+;;; Bug: this code doesn't recover well if the re doesn't match;
+;;; it will issue a warning though.
 
 (defun map-data-buffer (regexp func)
   "Call a mapping function on every entry in the data buffer (as named by +data-buffer+); returns a list of the results.  The function receives either a string, or if there are matching parentheses in the regexp, it will receive a list of the parenthesis matches."
 
-  (save-current-buffer
-    (set-buffer +data-buffer+)
-    (goto-char 0)
+  ;; New values are consed to the front of the list, and we reverse
+  ;; the final result to get the right order
+  (let ((accumulator nil)
+	;; Are we processing substrings or the whole match?
+	(match-count (count-regexp-string "\\\\(" regexp)))
 
-    ;; New values are consed to the front of the list, and we reverse
-    ;; the final result to get the right order
-    (let ((accumulator nil)
-	  ;; Are we processing substrings or the whole match?
-	  (match-count (count-regexp-string "\\\\(" regexp)))
-
-      (save-current-buffer
-	(set-buffer +data-buffer+)
-	(goto-char 0)
-	(while (re-search-forward regexp nil t)
-	  (push
-	   (if (zerop match-count)
-	       ;; Passing the whole match string to the function
-	       (funcall func (buffer-substring-no-properties (match-beginning 0) (match-end 0)))
-	     ;; Or an assembled list of substrings
-	     (let ((result nil))
-	       (dotimes (k match-count)
-		 (push (buffer-substring-no-properties (match-beginning (1+ k)) (match-end (1+ k))) result))
-	       (apply func (nreverse result))))
-	   ;; push onto accumulator
-	   accumulator)
-	  ;; Step past the end of the match to make sure we don't match again
-	  (goto-char (1+ (match-end 0)))))
+    (save-current-buffer
+      (set-buffer +data-buffer+)
+      (goto-char 0)
+      (while (re-search-forward regexp nil t)
+	(push
+	 (if (zerop match-count)
+	     ;; Passing the whole match string to the function
+	     (funcall func (buffer-substring-no-properties (match-beginning 0) (match-end 0)))
+	   ;; Or an assembled list of substrings
+	   (let ((result nil))
+	     (dotimes (k match-count)
+	       (push (buffer-substring-no-properties (match-beginning (1+ k)) (match-end (1+ k))) result))
+	     (apply func (nreverse result))))
+	 ;; push onto accumulator
+	 accumulator)
+	;; Step past the end of the match to make sure we don't match again
+	(goto-char (1+ (match-end 0))))
       (unless (eql (point) (buffer-end 1))
-	(warn "Warning: map finished early at %d" (point)))
-      (nreverse accumulator))))
+	(warn "Warning: map finished early at %d" (point))))
+    (nreverse accumulator)))
 
 
 
